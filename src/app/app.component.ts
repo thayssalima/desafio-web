515cad9affe842784458cb9964dab692dd183c2a
thayssa.lima <thayssa.mayara@cinbesa.com.br>
2023-10-20 16:05:27 -0300

    feat: Erros no material

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Livro } from './models/livro';
 import { Component, OnInit } from '@angular/core';
 import { LivroService } from './services/livro.service';
 import { NgForm } from '@angular/forms';
+import {MatDialog} from '@angular/material/dialog';
 
 @Component({
   selector: 'app-root',
@@ -12,7 +13,8 @@ export class AppComponent implements OnInit{
   livro = {} as Livro;
   livros: Livro[]=[];
 
-  constructor(private livroService: LivroService) {}
+  constructor(private livroService: LivroService,
+              public dialog: MatDialog) {}
 
   ngOnInit() {
     this.getLivros();
@@ -21,9 +23,14 @@ export class AppComponent implements OnInit{
   // define se um livro será criado ou atualizado
   saveLivro(form: NgForm) {
     if (this.livro.id !== undefined) {
-      this.livroService.editar(this.livro).subscribe(() => {
-        this.cleanForm(form);
-      });
+      this.livroService.editar(this.livro).subscribe({
+        next: (v) => {
+          this.cleanForm(form);
+        },
+        error: (e) => console.error(e),
+        complete: () => console.info('complete')
+    });
+
     } else {
       this.livroService.cadastrar(this.livro).subscribe(() => {
         this.cleanForm(form);
@@ -56,5 +63,13 @@ export class AppComponent implements OnInit{
     this.getLivros();
     form.resetForm();
     this.livro = {} as Livro;
+  }
+
+  onError(errorMsg: string){
+    this.dialog.open(DialogDataExampleDialog),{
+      data: {
+        animal: 'panda'
+      }
+    });
   }
 }
